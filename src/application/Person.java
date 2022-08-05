af0da53b0e2bbc3776e00a2ea32c36e5ba4d2175
jaxon.sloan <jaxon.sloan@IC-CS233-2.uc.ucalgary.ca>
2022-08-05 02:15:07 -0600

    Added error label

--- src/application/Person.java
+++ src/application/Person.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
@@ -18,8 +19,13 @@ public class Person {
 	
 	public HBox createHBoxField() {
 		HBox currentHBox = new HBox();
-		TextField currentTextfield = new TextField();
-		currentHBox.getChildren().add(currentTextfield);
+		Label nameLabel = new Label();
+		Label incomeLabel = new Label();
+		TextField incomeTextfield = new TextField(); 
+		TextField nameTextfield = new TextField();
+		nameLabel.setText("  Name:  ");
+		incomeTextfield.setText("   Annual Income:   ");
+		currentHBox.getChildren().addAll(nameLabel,nameTextfield,incomeLabel, incomeTextfield);
 		return currentHBox;
 	}
 	
