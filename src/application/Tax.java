7e24b9b5ae1d57aad1ecb461aea11d455123b1cf
jaxon.sloan <jaxon.sloan@IC-CS233-2.uc.ucalgary.ca>
2022-08-04 22:21:25 -0600

    Added the calculateAfterTaxIncome() method

--- src/application/Tax.java
+++ src/application/Tax.java
@@ -9,6 +9,34 @@ public class Tax {
 ;
 	}
 	
+	
+	private double calculateAfterTaxIncome() {
+		double btIncome = 0;
+		btIncome += this.getBeforeTaxIncome();
+		double totalTaxes = 0;
+			if (btIncome > 216511) {
+				totalTaxes += (btIncome - 216511) * 0.33;
+				btIncome = 216511;
+			}
+			if (btIncome > 151978) {
+				totalTaxes += (btIncome - 151978) * 0.29;
+				btIncome = 151978;
+			}
+			if (btIncome > 98040) {
+				totalTaxes += (btIncome - 98040) * 0.26;
+				btIncome = 98040;
+			}
+			if (btIncome > 49020) {
+				totalTaxes += (btIncome - 49020) * 0.205;
+				btIncome = 49020;
+			}
+			if (btIncome >= 49020) {
+				totalTaxes += btIncome * 0.29;
+				btIncome = 151978;
+			}
+		return totalTaxes;
+	}
+	
 	private double getBeforeTaxIncome() {
 		return beforeTaxIncome;
 	}
