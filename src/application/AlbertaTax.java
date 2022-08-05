2222caa7af0f95057474e901a200e2ebd6cfd07f
jaxon.sloan <jaxon.sloan@IC-CS233-2.uc.ucalgary.ca>
2022-08-04 23:01:10 -0600

    Adds calculateAFterTaxIncome() to AlbertaTax child class

--- src/application/AlbertaTax.java
+++ src/application/AlbertaTax.java
@@ -2,8 +2,43 @@ package application;
 
 public class AlbertaTax extends Tax {
 	
+	private double beforeTaxIncome;
+	private double afterTaxIncome;
+	
 	public AlbertaTax() {
 		
 	}
+	
+	private double calculateAfterTaxIncome() {
+		double btIncome = 0;
+		btIncome += this.getBeforeTaxIncome();
+		double totalTaxes = 0;
+		double currentBracket = 314928;
+		if (btIncome > currentBracket) {
+			totalTaxes += (btIncome - currentBracket) * 0.15;
+			btIncome = currentBracket;
+		}
+		currentBracket = 209952;
+		if (btIncome > currentBracket) {
+			totalTaxes += (btIncome - currentBracket) * 0.14;
+			btIncome = currentBracket;
+		}
+		currentBracket = 157464;
+			if (btIncome > currentBracket) {
+			totalTaxes += (btIncome - currentBracket) * 0.13;
+			btIncome = currentBracket;
+			}
+		currentBracket = 131220;
+		if (btIncome > currentBracket) {
+			totalTaxes += (btIncome - currentBracket) * 0.12;
+			btIncome = currentBracket;
+			}
+
+		if (btIncome <= currentBracket) {
+			totalTaxes += btIncome * 0.10;
+			}
+		return totalTaxes;
+	}
+	
 
 }
