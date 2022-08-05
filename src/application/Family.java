0af1ff09b1e6cef9a98931c2fbd5f919e50f5752
jaxon.sloan <jaxon.sloan@IC-CS233-2.uc.ucalgary.ca>
2022-08-05 07:00:55 -0600

    Adds javadoc.

--- src/application/Family.java
+++ src/application/Family.java
@@ -10,18 +10,35 @@ public class Family {
 		this.clearPeopleList();
 	}
 	
+	/** 
+	 * Creates a fresh new ArrayList and uses a setter method to replace the old peopleList.
+	 */
 	public void clearPeopleList() {
 		this.setPeopleList(new ArrayList<Person>());
 	}
 	
+	/**
+	 * Adds a new person onto the family, at the end of the ArrayList.
+	 * @param currentPerson Person object to be added.
+	 */
+	
 	public void addPersonToFamily(Person currentPerson) {
 		this.getPeopleList().add(currentPerson);
 	}
+	
+	/**
+	 * peopleList getter method.
+	 * @return peopleList
+	 */
 
-	ArrayList<Person> getPeopleList() {
+	public ArrayList<Person> getPeopleList() {
 		return peopleList;
 	}
 
+	/**
+	 * peopleList setter method. Private.
+	 * @param peopleList to be set.
+	 */
 	private void setPeopleList(ArrayList<Person> peopleList) {
 		this.peopleList = peopleList;
 	}
